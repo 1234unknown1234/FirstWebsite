df3cb3f7952d2acd41929fc3e8a9a3d1a03f7f5f
1234unknown1234 <cloud1234storage@gmail.com>
2024-07-26 20:43:30 +0430

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const addSemesterClickEvent = (semester) => {
+        semester.addEventListener('click', () => {
+            const url = semester.dataset.url;
+            if (url) {
+                window.location.href = url;
+            }
+        });
+    };
+
+    document.querySelectorAll('.semester').forEach(addSemesterClickEvent);
+});
